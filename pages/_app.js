4c3c231db3365555258e5fb764ff4f9e4a8b9bcf
nachoddiaz <nachoddiaz@LAPTOP-UUCHMGTT.localdomain>
2022-10-20 19:50:21 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
+import Head from "next/head"
 
-function MyApp({Component , pageProps}) {
-  return(
-    <MoralisProvider initializeOnMount = {false}>
-      <Header />
-      <Component {...pageProps} />
-    </MoralisProvider>
-  )
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT MarketPlace</title>
+                <meta name="description" content="Home page of the NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider /* appId={APP_ID} serverUrl={SERVER_URL} */ >
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
 import Header from "../components/Header"
 
-  
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>NFT MarketPlace</title>
-        <meta name="description" content="Home page of the NFT Marketplace" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Home Page
+    /*
+     * @notice: We need to show the recently listed NFTs
+     * @dev: We have many options:
+     *       Create a on-chain array that stores each listing
+     *       Take the event emited by the ListItem function, index it off-chain and read it from that server
+     * @dev: Decide to take the second option because is cheaper and more flexible
+     */
 
-      <Header>
-
-      </Header>
-    </div>
-  )
+    return <div className={styles.container}>Home Page</div>
 }
